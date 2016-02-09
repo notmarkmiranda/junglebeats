b3868cf8cd66e787206224c628ccb070ba6691a1
Mark Miranda <notmarkmiranda@gmail.com>
2016-02-09 05:50:08 -0700

    add an append method

--- linked_list.rb
+++ linked_list.rb
@@ -3,4 +3,14 @@ class LinkedList
     @head = nil
     @tail = @head
   end
+
+  def append(data)
+    node = New.node(data)
+    if @tail
+      @tail.next = node
+      @tail = node
+    else
+      @head, @tail = node
+    end
+  end
 end
